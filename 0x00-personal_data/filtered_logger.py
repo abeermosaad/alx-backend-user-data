4c6bf89a7403130cd0fb12fc9f902a67386a8e94
abeermosaad <abeermosaad69@gmail.com>
2024-02-16 20:53:06 +0200

    [Edit] Type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Regex module """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: List[str], separator) -> str:
     """Returns the log message obfuscated """
     new_msg = message
     for field in fields:
