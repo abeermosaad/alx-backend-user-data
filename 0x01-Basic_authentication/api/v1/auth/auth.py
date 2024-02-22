c293efb47b08f767ba5cc90bbeb18ae03c6215b5
abeermosaad <abeermosaad69@gmail.com>
2024-02-22 21:18:15 +0200

    [Add] Auth class template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    def __init__(self) -> None:
+        """init method"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user method"""
+        return None
