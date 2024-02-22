c6c4644e706d7f64cc32bc9f4b9bc13d9f19a8d8
abeermosaad <abeermosaad69@gmail.com>
2024-02-22 22:51:54 +0200

    [Add] Basic auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,6 +18,9 @@ if auth_type:
     if auth_type == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
+    if auth_type == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Modules of BasicAuth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class"""
+
+    def __init__(self) -> None:
+        """init method"""
+        pass
