0c8a29046612066a0d642d54a654c66764cdc963
abeermosaad <abeermosaad69@gmail.com>
2024-02-23 03:59:52 +0200

    [Add]  Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Modules of BasicAuth class"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -17,8 +19,10 @@ class BasicAuth(Auth):
         if not authorization_header or \
                 not isinstance(authorization_header, str):
             return None
+
         if authorization_header[:6] != 'Basic ':
             return None
+
         return authorization_header[6:]
 
     def decode_base64_authorization_header(
@@ -27,6 +31,7 @@ class BasicAuth(Auth):
         if not base64_authorization_header or \
                 not isinstance(base64_authorization_header, str):
             return None
+
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_string = decoded_bytes.decode('utf-8')
@@ -43,3 +48,20 @@ class BasicAuth(Auth):
                 ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials method"""
+        if not user_email or not isinstance(user_email, str):
+            return None
+
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        user = User()
+        valid_users = user.search({"email": user_email})
+
+        if valid_users:
+            if valid_users[0].is_valid_password(user_pwd):
+                return valid_users[0]
+        return None
