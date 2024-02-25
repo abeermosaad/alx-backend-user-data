760cdba2ed2b4089e7e5bc7f872683dd92e76c45
abeermosaad <abeermosaad69@gmail.com>
2024-02-25 22:34:00 +0200

    [Add] User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,6 +21,16 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
 
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id:
+            return None
+
+        if not isinstance(session_id, str):
+            return None
+        print(self.user_id_by_session_id)
+
+        return self.user_id_by_session_id.get(session_id)
